3c17d65c2cd1659a2ca64dc52473cf5fc766c59d
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-01-01 16:29:36 +0100

    removes inital angular template

--- ng-ftn/src/app/app.component.spec.ts
+++ ng-ftn/src/app/app.component.spec.ts
@@ -14,22 +14,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create component', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ng-ftn'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-ftn');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-ftn app is running!');
   });
 });
