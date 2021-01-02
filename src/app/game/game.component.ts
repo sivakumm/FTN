fccec628e3da91591ad2e75271d14e24474d0cb6
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-01-02 16:49:42 +0100

    initial announcement layout

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ISetting } from '../interfaces/setting.interface';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  settings!: ISetting;
+  round = 1;
+  cards = 26;
+  playerIdx = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    const storage = localStorage.getItem('gameSetting');
+    if (storage !== null) {
+      this.settings = JSON.parse(storage);
+    }
   }
 
+  choises(): number[] {
+    return Array.from({ length: this.cards + 1 }, (_, idx) => idx);
+  }
 }
