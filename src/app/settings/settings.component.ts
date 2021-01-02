3cc969b146a3a12eedbf4e9922b211bdab99158a
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-01-02 10:09:34 +0100

    adds basic form

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  players = [
+    { name: '' },
+    { name: '' },
+    { name: '' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
