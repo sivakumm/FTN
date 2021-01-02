d76790124ccdde44e51f0db740f535e6d1034d13
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-01-02 15:56:22 +0100

    renames basic test

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -19,7 +19,7 @@ describe('GameComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
